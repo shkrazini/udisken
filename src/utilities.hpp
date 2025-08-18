cecf1ef76e5b3d0387e498e24d9ef100616b41b6
BlackMa9ick <blackma9ick@proton.me>
2025-08-18 02:48:11 +0100

    refactor: add nodiscard to util functions

--- src/utilities.hpp
+++ src/utilities.hpp
@@ -53,15 +53,17 @@ auto Notify(const Notification& notification) -> bool;
 /// Checks if the string view has a non-zero value.
 ///
 /// @param sv Typical string view.
+///
 /// @return True if string view is non-empty (regardless of the original
 /// string's content), and its content contains characters other than '0'.
-constexpr auto NonZero(std::string_view sv) -> bool;
+[[nodiscard]] constexpr auto NonZero(std::string_view sv) -> bool;
 
 /// Checks if the environment variable is defined and has a non-zero value.
 ///
 /// @param var Name of the environment variable.
+///
 /// @return True if the environment variable is defined and is non-zero.
-auto NonZeroEnvironmentVariable(const std::string& var) -> bool;
+[[nodiscard]] auto NonZeroEnvironmentVariable(const std::string& var) -> bool;
 
 }  // namespace utils
 
